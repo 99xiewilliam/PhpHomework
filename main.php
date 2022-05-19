a3dc3ecba6cb0fef0daca1c9004c5b2e13e5d215
99xiewilliam <2725642450@qq.com>
2022-05-19 12:30:32 +0800

    modify db admin again

--- main.php
+++ main.php
@@ -99,180 +99,11 @@ while ($index < count($arr_id)) {
             <div id="sumPrice">Total Price: </div>
             <div id="no_goods">
             </div>
-<!--            <input type="button" value="check out">-->
+            <!--            <input type="button" value="check out">-->
             <form id="cate_insert" method="POST" action="admin-process.php?action=<?php echo ($action = 'logout'); ?>"
                   enctype="multipart/form-data">
                 <input type="submit" value="log out" />
-                <i<?php
-                require __DIR__.'/lib/db.inc.php';
-                if (ierg4210_auth() == false) {
-                    header('Location: login.php', true, 302);
-                    exit();
-                }
-                $res = ierg4210_cat_fetchall();
-
-                $products = '';
-                $arr_id = array();
-                $arr_name = array();
-                $count = 0;
-                while ($value = $res->fetch_array()) {
-                    $arr_id[$count] = $value["catid"];
-                    $arr_name[$count] = $value["name"];
-                    $count++;
-                }
-                $count = 0;
-                $que = ierg4210_prod_fetchall_by_catid(1);
-
-
-
-                $index = 0;
-                while ($index < count($arr_id)) {
-                    $in = $arr_id[$index];
-                    $products .= '
-                    <div class="cate_item" onmouseover="showNaviSubItems(this);">
-                        <h3>
-                            <a href="#">'.$arr_name[$index].'</a>
-                        </h3>
-                        <div class="sub_cate_box">
-                            <div class="sub_cate_items">';
-
-                    $que = ierg4210_prod_fetchall_by_catid($in);
-                    foreach ($que as $value1) {
-                        $products .= '<div>
-                                    <a href="#">'.$value1["name"].'</a>
-                                </div>';
-
-                        $products_detail .= '
-        <div class="product">
-            <div class="product-iWrap">
-                <!--page-->
-                <div class="productImg-wrap">
-                    <a class="productImg" href="./detail.php?pictureid='. $value1["pid"] .'&name='.$value1["name"].'">
-                        <img src="/static/images/'. $value1["pid"] .'.jpg">
-                    </a>
-                </div>
-                <!--price-->
-                <p class="productPrice">
-                    <em><b>¥</b>'.$value1["price"].'</em>
-                </p>
-                <!--title-->
-                <p class="productTitle">
-                    <a href="./detail.html"> '.$value1["name"].' </a>
-                </p>
-<!--                <input type="button" value="add cart" class="productStatus">-->
-            </div>
-        </div>
-        ';
-                    }
-//    while ($value1 = $que->fetch_array()) {
-//
-//    }
-
-
-                    $products .= '</div>
-              </div>
-          </div>';
-                    $index++;
-                }
-                ?>
-
-                <!DOCTYPE HTMl>
-                <html>
-                <head>
-                    <title>Home</title>
-                    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-                    <link rel="stylesheet" href="./static/css/common.css" />
-                    <link rel="stylesheet" href="./static/css/jquery.pagination.css" />
-                    <script type="text/javascript" src="./static/js/jquery.min.js"></script>
-                    <script type="text/javascript" src="./functions.js"></script>
-                    <script type="text/javascript" src="./static/js/jquery.pagination.js"></script>
-                </head>
-                <body>
-                <div id="top">
-
-                </div>
-                <div class="clear"></div>
-
-                <!-- car -->
-                <div id="top_main">
-                    <div id="settle_up" class="lr">
-                        <div class="settle_up_mt" onmouseover="showElementById('settle_up_items', true);" onmouseout="showElementById('settle_up_items',false);">
-                            shopping car
-                            <b></b>
-                        </div>
-                        <div id="settle_up_items" onmouseover="showElementById('settle_up_items', true);" onmouseout="showElementById('settle_up_items',false);">
-                            <div id="sumPrice">Total Price: </div>
-                            <div id="no_goods">
-                            </div>
-                            <!--            <input type="button" value="check out">-->
-                            <form id="cate_insert" method="POST" action="admin-process.php?action=<?php echo ($action = 'logout'); ?>"
-                                  enctype="multipart/form-data">
-                                <input type="submit" value="log out" />
-                                <input type="hidden" name="nonce" value="<?php echo csrf_getNonce($action); ?>">
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <a href="./showOrder.php" style="color: #a50a2c">go to orders table!!!</a>
-                <!-- nav -->
-                <div id="nav">
-                    <div id="category">
-                        <div id="cate_mt" onmouseover="showElementById('all_cate',true);"  onmouseout="showElementById('all_cate',false);">
-                            <a href="#">all categories</a>
-                            <span></span>
-                        </div>
-                        <div id="all_cate" onmouseover="showElementById('all_cate', true);" onmouseout="showElementById('all_cate', false);">
-                            <?php echo $products; ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="clear"></div>
-
-                <!-- main -->
-                <div id="main">
-                    <div class="bread-crumb">
-                        <a href="#">Home</a>
-                        <span>&gt;</span>
-                        <a href="#">Category1</a>
-                    </div>
-                    <div class="view grid-nosku">
-
-                        <?php echo $products_detail; ?>
-                    </div>
-                    <div class="pagination"></div>
-                </div>
-                <script>
-                    $('.pagination').pagination();
-                    // paypal.Buttons({
-                    //     // Sets up the transaction when a payment button is clicked
-                    //     createOrder: (data, actions) => {
-                    //         return actions.order.create({
-                    //             purchase_units: [{
-                    //                 amount: {
-                    //                     value: '77.44' // Can also reference a variable or function
-                    //                 }
-                    //             }]
-                    //         });
-                    //     },
-                    //     // Finalize the transaction after payer approval
-                    //     onApprove: (data, actions) => {
-                    //         return actions.order.capture().then(function(orderData) {
-                    //             // Successful capture! For dev/demo purposes:
-                    //             console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-                    //             const transaction = orderData.purchase_units[0].payments.captures[0];
-                    //             alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
-                    //             // When ready to go live, remove the alert and show a success message within this page. For example:
-                    //             // const element = document.getElementById('paypal-button-container');
-                    //             // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-                    //             // Or go to another URL:  actions.redirect('thank_you.html');
-                    //         });
-                    //     }
-                    // }).render('#paypal-button-container');
-                </script>
-                </body>
-                </html>
-
-                nput type="hidden" name="nonce" value="<?php echo csrf_getNonce($action); ?>">
+                <input type="hidden" name="nonce" value="<?php echo csrf_getNonce($action); ?>">
             </form>
         </div>
     </div>
@@ -306,7 +137,7 @@ while ($index < count($arr_id)) {
     <div class="pagination"></div>
 </div>
 <script>
-     $('.pagination').pagination();
+    $('.pagination').pagination();
     // paypal.Buttons({
     //     // Sets up the transaction when a payment button is clicked
     //     createOrder: (data, actions) => {
